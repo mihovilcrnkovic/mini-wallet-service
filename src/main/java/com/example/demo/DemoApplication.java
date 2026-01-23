b1b45a84560d0a7b1b7bef676e316f82dfbadedc
mihovilcrnkovic <mihovil.crnkovic@sigit.hr>
2026-01-23 10:42:20 +0100

    tmp flyway bean check

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner flywayCheck(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Flyway bean present: " +
+					ctx.containsBean("flyway"));
+		};
 	}
 
 }
